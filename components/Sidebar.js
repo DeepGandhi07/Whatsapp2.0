5965b1f4fbacadf49b34044beb7ef3fca7c1ec8c
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-02-09 13:21:49 +0530

    Submit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,52 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
 import styled from "styled-components";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
 const sidebar = () => {
   return (
-    <container>
-      <Header>Sidebar</Header>
-    </container>
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in Chats" />
+      </Search>
+    </Container>
   );
 };
 
 export default sidebar;
-const container = styled.div``;
-const Header = styled.div``;
-const UserAvatar = styled(Avatar);
+const Container = styled.div``;
+const Search = styled.div``;
+const SearchInput = styled.input``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+const IconsContainer = styled.div``;
